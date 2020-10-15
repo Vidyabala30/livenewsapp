67f5645a258940725cd5859c7205c97a334767f7
Vidyabala30 <72880235+Vidyabala30@users.noreply.github.com>
2020-10-15 13:53:06 +0530

    Update eachArticle.js

--- src/Components/eachArticle.js
+++ src/Components/eachArticle.js
@@ -23,22 +23,22 @@ export default class eachArticle extends Component {
                 <Segment>
                         <Image class="center"src={this.state.article.urlToImage}size=' <Segment>small' centered />
                 </Segment>
-                    <Header as="h1"  color='black'>{this.state.article.title}</Header>
+                <Header as="h1"  color='black'>{this.state.article.title}</Header>
                         <List.Description style={{ margin: "20px 0" ,color: "cadetblue"  }}>{this.state.article.description}
-                        </List.Description>
-                        <List bulleted horizontal>
-                        <List.Item>
-                         <p style={{color:"cadetblue"}}> {this.state.article.source.name}</p>
-                    </List.Item>
-                    <List.Item >
-                            {this.state.article.publishedAt.split("T")[0]}</List.Item>
+                  </List.Description>
+                       <List bulleted horizontal>
+                       <List.Item>
+                       <p style={{color:"cadetblue"}}> {this.state.article.source.name}</p>
+                   </List.Item>
+                   <List.Item >
+                        {this.state.article.publishedAt.split("T")[0]}</List.Item>
                     </List>
                     <div>
-                    <Button  floated='left' color="blue"   onClick = {this.handleClick}>
+                      <Button  floated='left' color="blue"   onClick = {this.handleClick}>
                       Back
                     </Button>
                  </div>
-         </div>
+           </div>
         )
     }
 } 
